eb0cbc62d0d7090d4012b073cfbb8edba189c396
shamsheer <shamsheer@ubuntu.localdomain>
2026-03-30 20:12:35 +0530

    main.tf jen-para files

---
+++ main.tf
@@ -0,0 +1,25 @@
+variable "environment" {
+  type = string
+  default = "dev"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "jenkins_demo_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    # This will name it dev-VPC, test-VPC, or prod-VPC
+    Name = "${var.environment}-VPC" 
+  }
+}
